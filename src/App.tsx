ca26c7fc703631068a9652cacd289a505fb93dac
JM <jm@merakisv.com>
2019-10-12 21:34:04 +0800

    added Ant Design with Customizable config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import { Button } from "antd";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      Hello
+      React Typescript, Ant Design Customizable, Redux
+      <Button type="primary">Primary Button</Button>
     </div>
   );
-}
+};
 
 export default App;
